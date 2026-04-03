479d0071f727be014bf7e727d3e8ebdc4a629751
riley <riley@websimulations.com>
2026-04-03 13:04:51 -0500

    Update version strings for build.

--- zephyr/tempo-bt-v1/include/generated/app_version.h
+++ zephyr/tempo-bt-v1/include/generated/app_version.h
@@ -6,12 +6,12 @@
 #ifndef APP_VERSION_H
 #define APP_VERSION_H
 
-#define APP_VERSION_STRING "1.3.0"
-#define APP_GIT_COMMIT "0dff03f-dirty"
-#define APP_BUILD_DATE "2026-03-03"
+#define APP_VERSION_STRING "1.4.0"
+#define APP_GIT_COMMIT "05a637a"
+#define APP_BUILD_DATE "2026-04-03"
 #define APP_DEVICE_TYPE "V2"
 #define APP_VERSION_MAJOR 1
-#define APP_VERSION_MINOR 3
+#define APP_VERSION_MINOR 4
 #define APP_VERSION_PATCH 0
 
 /* Numeric version for Tempo (1xx) vs Dropkick (0xx) differentiation */
